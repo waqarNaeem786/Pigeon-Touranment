d922a72f0149eab085e925feb62b6211083ce38d
pikes <neutrone04@gmail.com>
2023-09-29 18:53:20 +0500

    final

--- src/Adminpanel.js
+++ src/Adminpanel.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { db, addDoc, collection, getDocs, doc, deleteDoc, updateDoc } from './firebase';
+import {
+    db,
+    addDoc,
+    collection,
+    getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+} from './firebase'; // Import Firebase-related functions and components
 
 export default function Adminpanel() {
     const [inputValue, setInputValue] = useState('');
     const [timeValue, setTimeValue] = useState('');
     const [pigeonCount, setPigeonCount] = useState('');
+    const [dateValue, setDateValue] = useState(''); // State for the date field
     const [data, setData] = useState([]);
     const [editItemId, setEditItemId] = useState(null); // Track the item being edited
+    const [ascendingOrder, setAscendingOrder] = useState(true); // Sort order
 
     const handleNameChange = (e) => {
         setInputValue(e.target.value);
-    }
+    };
 
     const handleTimeChange = (e) => {
         setTimeValue(e.target.value);
-    }
+    };
 
     const handlePigeonCountChange = (e) => {
         setPigeonCount(e.target.value);
-    }
+    };
+
+    const handleDateChange = (e) => {
+        setDateValue(e.target.value);
+    };
 
     const sendDataToFirestore = async () => {
-        if (!inputValue || !timeValue || !pigeonCount) {
-            alert("All fields must be filled out.");
+        if (!inputValue || !timeValue || !pigeonCount || !dateValue) {
+            alert('All fields must be filled out.');
             return;
         }
 
@@ -33,6 +47,7 @@ export default function Adminpanel() {
                     name: inputValue,
                     time: timeValue,
                     pigeonCount: pigeonCount,
+                    date: dateValue, // Include the date in the update
                 });
                 setEditItemId(null); // Clear edit state
             } else {
@@ -41,17 +56,19 @@ export default function Adminpanel() {
                     name: inputValue,
                     time: timeValue,
                     pigeonCount: pigeonCount,
+                    date: dateValue, // Include the date when adding
                 });
             }
 
             setInputValue('');
             setTimeValue('');
             setPigeonCount('');
+            setDateValue('');
 
             // Fetch updated data
             fetchData();
         } catch (error) {
-            console.error("Error adding/updating data: ", error);
+            console.error('Error adding/updating data: ', error);
         }
     };
 
@@ -62,9 +79,21 @@ export default function Adminpanel() {
             querySnapshot.forEach((doc) => {
                 docs.push({ id: doc.id, ...doc.data() });
             });
-            // Sort data by time in descending order (users with most time first)
-            docs.sort((a, b) => b.time - a.time);
-            setData(docs);
+
+            // Sort data based on the current sort order and time field
+            docs.sort((a, b) => {
+                const aTime = parseInt(a.time);
+                const bTime = parseInt(b.time);
+                return ascendingOrder ? aTime - bTime : bTime - aTime;
+            });
+
+            // Add serial numbers
+            const dataWithSerialNumbers = docs.map((item, index) => ({
+                ...item,
+                serialNumber: index + 1,
+            }));
+
+            setData(dataWithSerialNumbers);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -74,11 +103,12 @@ export default function Adminpanel() {
         fetchData();
     }, []);
 
-    const handleEdit = (id, name, time, pigeonCount) => {
+    const handleEdit = (id, name, time, pigeonCount, date) => {
         setEditItemId(id);
         setInputValue(name);
         setTimeValue(time);
         setPigeonCount(pigeonCount);
+        setDateValue(date);
     };
 
     const handleDelete = async (id) => {
@@ -97,6 +127,19 @@ export default function Adminpanel() {
                     <div className="card">
                         <div className="card-body">
                             <h2 className="card-title">Add/Edit Data</h2>
+
+                            {/* Add the date input field */}
+                            <div className="form-group">
+                                <label htmlFor="date">Date</label>
+                                <input
+                                    type="text"
+                                    id="date"
+                                    className="form-control"
+                                    value={dateValue}
+                                    onChange={handleDateChange}
+                                />
+                            </div>
+
                             <div className="form-group">
                                 <label htmlFor="userName">Name</label>
                                 <input
@@ -127,7 +170,10 @@ export default function Adminpanel() {
                                     onChange={handlePigeonCountChange}
                                 />
                             </div>
-                            <button className="btn btn-primary" onClick={sendDataToFirestore}>
+                            <button
+                                className="btn btn-primary"
+                                onClick={sendDataToFirestore}
+                            >
                                 {editItemId ? 'Update' : 'Submit'}
                             </button>
                         </div>
@@ -136,35 +182,47 @@ export default function Adminpanel() {
                 <div className="col-md-8">
                     <table className="table table-striped">
                         <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Pigeons</th>
-                            <th>Time</th>
-                            <th>Action</th>
-                        </tr>
+                            <tr>
+                                <th>Serial No</th>
+                                <th>Date</th>
+                                <th>Name</th>
+                                <th>Pigeons</th>
+                                <th>Time</th>
+                                <th>Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        {data.map((item) => (
-                            <tr key={item.id}>
-                                <td>{item.name}</td>
-                                <td>{item.pigeonCount}</td>
-                                <td>{item.time}</td>
-                                <td>
-                                    <button
-                                        className="btn btn-warning btn-sm mr-2"
-                                        onClick={() => handleEdit(item.id, item.name, item.time, item.pigeonCount)}
-                                    >
-                                        Edit
-                                    </button>
-                                    <button
-                                        className="btn btn-danger btn-sm"
-                                        onClick={() => handleDelete(item.id)}
-                                    >
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
+                            {data.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.serialNumber}</td>
+                                    <td>{item.date}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.pigeonCount}</td>
+                                    <td>{item.time}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-warning btn-sm mr-2"
+                                            onClick={() =>
+                                                handleEdit(
+                                                    item.id,
+                                                    item.name,
+                                                    item.time,
+                                                    item.pigeonCount,
+                                                    item.date
+                                                )
+                                            }
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            className="btn btn-danger btn-sm"
+                                            onClick={() => handleDelete(item.id)}
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
--- src/Mainpage.js
+++ src/Mainpage.js
@@ -6,7 +6,8 @@ import image1 from "./image1.jpeg";
 import image2 from "./image2.jpeg";
 
 function MainPage() {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
+    const [firstPosition, setFirstPosition] = useState(null);
 
     useEffect(() => {
         // Fetch data from Firestore 'userData' collection
@@ -17,18 +18,45 @@ function MainPage() {
                 querySnapshot.forEach((doc) => {
                     docs.push(doc.data());
                 });
-                setData(docs);
+
+                // Group data by date
+                const groupedData = {};
+                docs.forEach((item) => {
+                    const date = item.date;
+                    if (!groupedData[date]) {
+                        groupedData[date] = [];
+                    }
+                    groupedData[date].push(item);
+                });
+
+                // Find the first position holder (highest time)
+                let highestTime = '0';
+                let highestTimeName = '';
+                Object.keys(groupedData).forEach((date) => {
+                    groupedData[date].sort((a, b) => {
+                        // Compare time values including the values on both sides of the period
+                        return b.time.localeCompare(a.time);
+                    });
+                    if (groupedData[date][0]) {
+                        if (groupedData[date][0].time.localeCompare(highestTime) > 0) {
+                            highestTime = groupedData[date][0].time;
+                            highestTimeName = groupedData[date][0].name;
+                        }
+                    }
+                });
+
+                setFirstPosition(highestTimeName);
+                setData(groupedData);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
 
         fetchData();
-    }, []);
+    }, []); // Run this effect only once
 
     return (
         <div className="container mt-4">
-
             {/* Image Slider */}
             <div className="row">
                 <div className="col-md-8 mx-auto">
@@ -44,31 +72,47 @@ function MainPage() {
                 </div>
             </div>
 
-            {/* Table to display Firestore data */}
-            <div className="row mt-4">
-                <div className="col-md-12">
-                    <div className="table-responsive">
-                        <table className="table table-bordered">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Pigeon Count</th> {/* Update the column header here */}
-                                <th>Time</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {data.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.name}</td>
-                                    <td>{item.pigeonCount}</td> {/* Update the property name here */}
-                                    <td>{item.time}</td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+            {/* Display the first position holder */}
+            {firstPosition && (
+                <div className="row mt-4">
+                    <div className="col-md-12 text-center">
+                        <h3 className="text-primary">
+                            First Position: {firstPosition}
+                        </h3>
                     </div>
                 </div>
-            </div>
+            )}
+
+            {/* Display tables for each date */}
+            {Object.keys(data).map((date, index) => (
+                <div className="row mt-4" key={index}>
+                    <div className="col-md-12">
+                        <h3>{date}</h3>
+                        <div className="table-responsive">
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Serial No</th>
+                                        <th>Name</th>
+                                        <th>Pigeon Count</th>
+                                        <th>Time</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data[date].map((item, subIndex) => (
+                                        <tr key={subIndex}>
+                                            <td>{subIndex + 1}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.pigeonCount}</td>
+                                            <td>{item.time}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
